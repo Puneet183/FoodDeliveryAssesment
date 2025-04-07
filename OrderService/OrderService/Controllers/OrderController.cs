1b2df1f5178515de8241d9219378f20d2c9b7fac
Puneet Verma <puneet.verman@reveleer.com>
2025-04-07 19:10:53 +0530

    minor fixes

--- OrderService/OrderService/Controllers/OrderController.cs
+++ OrderService/OrderService/Controllers/OrderController.cs
@@ -1,11 +1,22 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderService.Interfaces;
+using OrderService.Models;
 
-namespace OrderService.Controllers
+[ApiController]
+[Route("api/order")]
+public class OrderController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class OrderController : ControllerBase
+    private readonly IRabbitMQProducer _producer;
+
+    public OrderController(IRabbitMQProducer producer)
     {
+        _producer = producer;
+    }
+
+    [HttpPost("place")]
+    public IActionResult PlaceOrder([FromBody] OrderDto order)
+    {
+        _producer.SendMessage(order);
+        return Ok("Order placed.");
     }
 }
--- OrderService/OrderService/Interfaces/IRabbitMQProducer.cs
+++ OrderService/OrderService/Interfaces/IRabbitMQProducer.cs
@@ -1,6 +1,9 @@
-﻿namespace OrderService.Interfaces
+﻿using OrderService.Models;
+
+namespace OrderService.Interfaces
 {
     public interface IRabbitMQProducer
     {
+        void SendMessage(OrderDto order);
     }
 }
